709a3cd4d82ef094cc9249c0c3b7a95a95111448
mathler574 <77964088+mathler574@users.noreply.github.com>
2022-12-09 16:43:16 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 const config = {
     
-    USERNAME: "User#0000",
+    USERNAME: "XDΞLΛ®#2415",
     /**
      * This can either be a file link (relative or external)
      * or a Gravatar email or hash.
